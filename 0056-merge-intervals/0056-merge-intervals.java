12ddb34f44442daaa72d70e97601a73e66539e6b
Saifullah Mosaad <109905783+Saifullah-1@users.noreply.github.com>
2024-07-13 09:26:31 +0300

    Time: 8 ms (84.34%), Space: 46.5 MB (46.74%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,32 +1,19 @@
 class Solution {
-    public void insertion_sort(int[][] arr) {
-        for (int i = 1; i < arr.length; ++i) {
-            int []curr = arr[i];
-            int j = i - 1;
-            while (j >= 0 && arr[j][0] > curr[0]) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            arr[j + 1] = curr;
-        }
-    }
-
     public int[][] merge(int[][] intervals) {
-        insertion_sort(intervals);
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         List<int[]> result = new ArrayList<>();
-        int[] curr = intervals[0];
+        int[] currentInterval = intervals[0];
 
         for (int i = 1; i < intervals.length; i++) {
-            if (curr[1] >= intervals[i][0]) {
-                curr[1] = Math.max(curr[1], intervals[i][1]);
+            if (currentInterval[1] >= intervals[i][0]) {
+                currentInterval[1] = Math.max(currentInterval[1], intervals[i][1]);
             } else {
-                result.add(curr);
-                curr = intervals[i];
+                result.add(currentInterval);
+                currentInterval = intervals[i];
             }
         }
-        result.add(curr);
+        result.add(currentInterval);
 
         return result.toArray(new int[result.size()][]);
     }
-
 }
