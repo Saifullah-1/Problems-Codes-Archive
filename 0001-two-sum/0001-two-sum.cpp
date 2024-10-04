5e0ce9fa6c36549737fe5d35c67324fb0fd441e8
Saifullah Mosaad <109905783+Saifullah-1@users.noreply.github.com>
2024-10-04 15:11:15 +0300

    Time: 13 ms (53.96%), Space: 15.2 MB (10.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int i = 0, j = 1;
-        while (i < j) {
-            while (j < nums.size()) {
-                if (nums[i] + nums[j] == target)
-                    return vector<int>{i, j};
-                j++;
+        // int i = 0, j = 1;
+        // while (i < j) {
+        //     while (j < nums.size()) {
+        //         if (nums[i] + nums[j] == target)
+        //             return vector<int>{i, j};
+        //         j++;
+        //     }
+        //     i++;
+        //     j = i + 1;
+        // }
+        // return vector<int>{i, j};
+        multimap<int, int> m;
+        for (int i = 0; i < nums.size(); ++i)
+            m.insert({nums[i], i});
+
+        vector<int> v(2);
+        for (auto p : m) {
+            auto it = m.find(target - p.first);
+            if (it != m.end() && it->second != p.second) {
+                v[0] = p.second;
+                v[1] = it->second;
+                break;
             }
-            i++;
-            j = i + 1;
         }
-        return vector<int>{i, j};
+        return v;
     }
 };
