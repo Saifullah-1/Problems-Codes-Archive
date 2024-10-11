dc2174b816c0b6a9779722e2e98b2f115f77d11e
Saifullah Mosaad <109905783+Saifullah-1@users.noreply.github.com>
2024-10-11 22:11:38 +0300

    Time: 63 ms (5.14%), Space: 48.8 MB (47.97%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        val,
+        toBe(val) {
+            if (val === this.val)
+                return true;
+            throw new Error("Not Equal");
+        },
+        notToBe(val) {
+            if (val !== this.val)
+                return true;
+            throw new Error("Equal");
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
